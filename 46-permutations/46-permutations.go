952304e044e7fde2bc174dd602573863d055f450
Nikita Shkaruba <sh.nickita@list.ru>
2022-02-13 20:38:34 +0300

    Time: 4 ms (35.19%), Space: 3.4 MB (11.04%) - LeetHub

---
+++ 46-permutations/46-permutations.go
@@ -0,0 +1,24 @@
+func permute(nums []int) [][]int {
+    if len(nums) <= 1 {
+      return [][]int{nums}
+    }
+    
+    ans := [][]int{}
+        
+    for i := 0; i < len(nums); i++ {
+        v := make([]int, len(nums))    
+        copy(v, nums)
+      
+        v[i], v[0] = v[0], v[i]
+        v = v[1:]
+    
+        res := permute(v)
+        
+        for _, j := range res {
+            j = append([]int{nums[i]}, j...)
+            ans = append(ans, j)
+        }
+    }
+    
+    return ans
+}
