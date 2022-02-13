70bbc8bae0b2627fb9fd3e114a1cec78691ad96a
Nikita Shkaruba <sh.nickita@list.ru>
2022-02-13 22:13:18 +0300

    Time: 0 ms (100.00%), Space: 2.4 MB (96.81%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+    dummy := ListNode{}
+    dummy.Next = nil
+    tail := &dummy
+    
+    for l1 != nil || l2 != nil {
+        if l1 == nil {
+            (*tail).Next = l2
+            break
+        } else if l2 == nil {
+            (*tail).Next = l1
+            break
+        } else {
+            if l1.Val <= l2.Val {
+                (*tail).Next = l1
+                l1 = (*l1).Next
+            } else {
+                (*tail).Next = l2
+                l2 = (*l2).Next
+            }
+            tail = tail.Next            
+        }
+    }
+
+    return dummy.Next
+}
