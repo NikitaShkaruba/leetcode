0c268a4eac54c52ed6539b7507b7998cb82172e5
Nikita Shkaruba <sh.nickita@list.ru>
2022-02-20 20:10:40 +0300

    Time: 28 ms (97.61%), Space: 6.7 MB (87.68%) - LeetHub

---
+++ 704-binary-search/704-binary-search.go
@@ -0,0 +1,35 @@
+/*
+
+  Solutions:
+    - Binary search: O(logn) time, O(1) space
+    
+  Test cases:
+    - [-1,0,3,5,9,12], 2 : -1
+    - [-1,0,3,5,9,12], 9 : 4 
+    - [1], 1 : 0 
+  
+  Testing grounds:
+    - [-1,0,3,5,9,12], 9 : 4 
+              m l r
+
+*/
+func search(nums []int, target int) int {
+  l := 0
+  r := len(nums) - 1
+  
+  for l <= r {
+    m := int(math.Floor(float64((r + l)) / 2))
+             
+    if nums[m] == target {
+      return m
+    }
+    
+    if nums[m] < target {
+      l = m + 1
+    } else {
+      r = m - 1
+    }
+  }
+  
+  return -1
+}
