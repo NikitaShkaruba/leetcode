7244a13c0da487efc20e7bd0e2d9350c99a568d9
Nikita Shkaruba <sh.nickita@list.ru>
2022-02-24 02:26:33 +0300

    Time: 18 ms (47.36%), Space: 9 MB (7.43%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.go
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/*
+  
+  Solutions:
+    - DFS with remembering path: O(n) time, O(n) space
+  
+  Test cases:
+    - root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+    - root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+    - root = [2,1], p = 2, q = 1
+  
+  Testing grounds:
+
+*/
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+  pPath, qPath := findPath(root, p, q, []*TreeNode{}, nil, nil)
+  
+  for len(pPath) != len(qPath) {
+    if len(pPath) > len(qPath) {
+      pPath = pPath[:len(pPath)-1]
+    } else {
+      qPath = qPath[:len(qPath)-1]
+    }  
+  }
+  
+  for pPath[len(pPath)-1] != qPath[len(qPath)-1] {
+    pPath = pPath[:len(pPath)-1]
+    qPath = qPath[:len(qPath)-1]
+  }
+  
+  return pPath[len(pPath)-1]
+}
+
+func findPath(node, p, q *TreeNode, path, pPath, qPath []*TreeNode) ([]*TreeNode, []*TreeNode) {
+  if node == nil {
+    return pPath, qPath
+  }
+  if pPath != nil && qPath != nil {
+    return pPath, qPath
+  }
+  
+  path = append(path, node)
+  
+  if node == p && pPath == nil {
+    pPath = copyPathSlice(path)
+  } else if node == q && qPath == nil {
+    qPath = copyPathSlice(path)
+  }
+  
+  pPath, qPath = findPath(node.Left, p, q, path, pPath, qPath)
+  pPath, qPath = findPath(node.Right, p, q, path, pPath, qPath)
+  
+  return pPath, qPath
+}
+
+func copyPathSlice(path []*TreeNode) []*TreeNode {
+  result := make([]*TreeNode, len(path))
+  
+  for i, p := range path {
+    result[i] = p
+  }
+  
+  return result
+} 
+
+
+
+
+
+
+
+
