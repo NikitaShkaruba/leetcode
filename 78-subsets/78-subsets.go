1f289fd4cfd4ab4b655d888a898e65156e9c814a
Nikita Shkaruba <sh.nickita@list.ru>
2022-03-02 03:28:10 +0300

    Time: 4 ms (19.15%), Space: 2.3 MB (85.21%) - LeetHub

---
+++ 78-subsets/78-subsets.go
@@ -0,0 +1,54 @@
+/*
+
+  Solutions:
+    - Brute force: O(n^n * n*logn) time, O(2^n) space
+    - Iterate and mutate with current subsets: O(n*2^n) time, O(2^n) space
+  
+  Test cases:
+    - [1,2,3] : [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+    - [0] : [[],[0]]
+  
+  Testing grounds:
+    [1,2,3]
+    
+    [] - [1] - [1, 2] - [1, 2, 3]
+             - [1, 3] - [1, 2, 3]
+       - [2] - [2, 1] - [1, 2, 3]
+             - [2, 3] - [1, 2, 3]
+       - [3] - [3, 1] - [1, 2, 3]
+             - [3, 2] - [1, 2, 3]
+     
+     nums = [1,2,3]
+     subsets = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+     Tsubset = [[], [1], [2], [1, 2]]
+     
+     nums = [1,2]
+     subsets = [[], [1], [2], [1, 2], [3]]
+     
+     nums = [0]
+             ^
+     subsets = [[], [0]]
+*/
+
+// Todo: check range + append
+func subsets(nums []int) [][]int {
+  subsets := [][]int{
+    []int{},
+  }
+  
+  for _, num := range nums {
+    subsetsCurLen := len(subsets)
+    
+    for i := 0; i < subsetsCurLen; i++ {
+      newSubset := make([]int, len(subsets[i]) + 1)
+      for j, v := range subsets[i] {
+        newSubset[j] = v
+      }
+      newSubset[len(newSubset)-1] = num
+      
+      subsets = append(subsets, newSubset)
+    }
+  }
+  
+  return subsets
+}
