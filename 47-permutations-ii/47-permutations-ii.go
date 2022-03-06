6e0802b960f4f3e98b2f8fac988fe8a30d9090b6
Nikita Shkaruba <sh.nickita@list.ru>
2022-03-06 04:56:43 +0300

    Time: 9 ms (42.27%), Space: 4 MB (56.01%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.go
+++ 47-permutations-ii/47-permutations-ii.go
@@ -2,7 +2,7 @@
 
   Solutions:
     1. Sort + backtrack: O(n^3) time, O(n!) space
-    2. Backtrack + hashtable: O(n^3) time, O(n!) space
+    2. Count all + bracktrack: O(n^2 * n!) time, O(n) space
     
   Test cases:
     - [1] : [[1]]
@@ -52,35 +52,34 @@
 */
 
 func permuteUnique(nums []int) [][]int {
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] < nums[j]
-	})
+  numAmounts := make(map[int]int)
+  for _, v := range nums {
+    if _, ok := numAmounts[v]; !ok {
+      numAmounts[v] = 0
+    }
+    
+    numAmounts[v]++
+  }
 
 	res := make([][]int, 0)
-	backtrack(nums, []int{}, &res)
+  backtrack(numAmounts, []int{}, len(nums), &res)
 
 	return res
 }
 
-func backtrack(nums []int, perm []int, res *[][]int) {
-	if len(nums) == 0 {
+func backtrack(numAmounts map[int]int, perm []int, finishedPermLen int, res *[][]int) {
+	if len(perm) == finishedPermLen {
 		*res = append(*res, append([]int{}, perm...))
 		return
 	}
 
-	for i := range nums {
-		if i >= 1 && nums[i-1] == nums[i] {
-			continue
-		}
-
-		numsWithoutI := make([]int, 0, len(nums)-1)
-		for j, v := range nums {
-			if j == i {
-				continue
-			}
-			numsWithoutI = append(numsWithoutI, v)
-		}
-
-		backtrack(numsWithoutI, append(perm, nums[i]), res)
+	for num, amount := range numAmounts {
+    if amount == 0 {
+      continue
+    }
+    
+    numAmounts[num]--
+    backtrack(numAmounts, append(perm, num), finishedPermLen, res)
+    numAmounts[num]++
 	}
 }
