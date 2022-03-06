70a21c73de2189ffd6deb31c488aed5eeb3ca690
Nikita Shkaruba <sh.nickita@list.ru>
2022-03-06 04:33:14 +0300

    Time: 7 ms (54.30%), Space: 4.1 MB (56.01%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.go
@@ -0,0 +1,88 @@
+/*
+
+  Solutions:
+    1. Sort + backtrack: O(n^3) time, O(n!) space
+    2. Backtrack + hashtable: O(n^3) time
+    
+  Test cases:
+    - [1] : [[1]]
+    - [1,1,2] : [[1,1,2],[1,2,1],[2,1,1]]
+    - [1,1,1,1,2,2]
+    - [1,2,3] : [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+  
+  Testing grounds:
+
+          [1,2,3], []
+          /          \
+    [2, 3], [1] -- [1, 3], [2]  -- [1, 2], [3]
+      |
+  [], [1, 2, 3] -- ...
+  
+          [1,2,3,2], []
+          /                \
+  [2,3,2], [1] -- [1,3,2], [2] -- [1,2,2], [3] -- [1,2,3], [2]
+  
+                       |                              |
+                    [], [2,1,3,2]                 [], [2,1,3,2]
+                    
+              [1,2,2,3], []
+          /                \
+  [2,2,3], [1] -- [1,2,3], [2] -- [1,2,3], [2] -- [1,2,2], [3]
+                      |               |
+                     ...             ...
+                      |               |
+                [], [2,1,3,2]     [], [2,1,3,2]
+                
+      [1], []
+         |
+      [], [1]
+      
+                  [1,2,1,3]
+       [1,2], [1]  -- [x] -- [1,1], [2]
+  [2], [1,1]  -- [1], [1,2]    [1], [2, 1] -- [x]
+  
+    [1,2,1,3,1]
+    [1,1,1,2,3], []
+    /
+    [1,1,2,3], [1] -- [1,1,1,3], [2] -- [1,1,1,2], [3]
+    
+    [1,1,1,2,3], []
+    
+    []
+*/
+
+func permuteUnique(nums []int) [][]int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+
+	res := make([][]int, 0)
+	backtrack(nums, []int{}, &res)
+
+	return res
+}
+
+func backtrack(nums []int, perm []int, res *[][]int) {
+	if len(nums) == 0 {
+		permCopy := make([]int, len(perm))
+		copy(permCopy, perm)
+		*res = append(*res, permCopy)
+		return
+	}
+
+	for i := range nums {
+		if i >= 1 && nums[i-1] == nums[i] {
+			continue
+		}
+
+		numsWithoutI := make([]int, 0, len(nums)-1)
+		for j := range nums {
+      if j == i {
+				continue
+			}
+			numsWithoutI = append(numsWithoutI, nums[j])
+		}
+		
+		backtrack(numsWithoutI, append(perm, nums[i]), res)
+	}
+}
