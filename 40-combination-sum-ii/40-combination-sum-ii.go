cccb5a4dfd3d9bf1942cda0b91b47a87a2dd72e7
Nikita Shkaruba <sh.nickita@list.ru>
2022-03-08 15:29:06 +0300

    Time: 10 ms (19.77%), Space: 2.5 MB (88.97%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.go
+++ 40-combination-sum-ii/40-combination-sum-ii.go
@@ -3,6 +3,7 @@
   Solutions:
     1. HashMap + backtracking + remove duplicates: O(a lot) time, O(a lot) space
     2. HashMap + backtracking: O(n + n^(t/l)) time, O(n + t/l + n^(t/l)) space
+    3. Sort + Backtracking (Coz everything is sorted, we can easily break out of iterations): O(n*logn + n^(t/l)) time, O(n + t/l + n^(t/l))
   
   Test cases:
     - [10,1,2,7,6,1,5] : [[1,1,6],[1,2,5],[1,7],[2,6]]
@@ -36,7 +37,7 @@ func combinationSum2(candidates []int, target int) [][]int {
   })
   
   res := make([][]int, 0)
-  backtrack(candidates, nil, target, &res)
+  backtrack(candidates, make([]int, 0, len(candidates)), target, &res)
   return res
 }
 
@@ -51,11 +52,14 @@ func backtrack(candidates, comb []int, target int, res *[][]int) {
   }
   
   for i, v := range candidates {
+    // Only go into the first occurencies of a number
     if i > 0 && candidates[i] == candidates[i-1] {
       continue
     }
     
     newTarget := target - v
+    
+    // Because everything is sorted, all the other numbers will ass well form newTarget < 0, so break preventivelly
     if newTarget < 0 {
       break
     }
