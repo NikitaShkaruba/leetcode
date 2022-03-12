84fd44be6a2f132e254eab00c54c5f5251afcc23
Nikita Shkaruba <sh.nickita@list.ru>
2022-03-13 02:47:37 +0300

    Time: 292 ms (87.78%), Space: 21 MB (72.78%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.go
@@ -0,0 +1,96 @@
+/*
+
+  Solutions:
+    - Brute force: O(n^n) time, O(n^n) space
+    - Backtrack: O(n^n) time, O(n^n) space
+  
+  Test cases:
+    - "aabaa": [["a","aba",a],["a","a","b","a","a"], ["a", "a", "b", "aa"], ["aa","b", "aa"],["aa","b", "a","a"], "aabaa"]
+    - "aab": [["a","a","b"],["aa","b"]]
+    - "a": [["a"]]
+  
+  Testng grounds:
+    - aca b aca
+    ^ 
+                    b:aa,aa
+              /                 \                  |              |                     |
+           aba:a,a            ab:a,aa            ba:aa,a       b,a:a,aa              b,a:aa,a
+        /   \      \              
+   aabaa   aaba:,a  abaa:a,
+   
+    [],abba
+      [a], bba
+        - [a, b], ba
+          - [a, b, b], a
+            - [a, b, b, a] (V)
+          - [a, b, ba] (X)
+        - [a, bb], a
+          - [a, bb, a] (V)
+        - [a, bba] (X) 
+      [ab], ba (X)
+      [abb], a (X)
+      [abba], 
+        - (V)
+  
+*/
+func partition(s string) [][]string {
+  res := [][]string{}
+  backtrack(s, []string{}, make(map[string]bool), &res)
+  return res
+}
+
+func backtrack(s string, comb []string, cache map[string]bool, res *[][]string) {
+  if len(s) == 0 {
+    *res = append(*res, append([]string{}, comb...)) 
+    return
+  }
+  
+  for i := 1; i <= len(s); i++ {
+    subS := s[0:i]
+    
+    isPali, ok := cache[subS]
+    if !ok {
+      isPali = isPalindrome(subS)
+      cache[subS] = isPali
+    }
+        
+    if !isPali {
+      continue 
+    }
+    
+    backtrack(s[i:], append(comb, subS), cache, res)
+  }
+}
+
+func isPalindrome(s string) bool {
+  l := 0
+  r := len(s)-1
+  
+  for l < r {
+    if s[l] != s[r] {
+      return false
+    }
+    
+    l++
+    r--
+  }
+  
+  return true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
