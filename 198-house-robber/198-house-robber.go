f62c79e33a165fe7b87a4b3e774b532e3e17d131
Nikita Shkaruba <sh.nickita@list.ru>
2022-03-13 15:36:04 +0300

    Time: 0 ms (100.00%), Space: 2 MB (52.86%) - LeetHub

---
+++ 198-house-robber/198-house-robber.go
@@ -0,0 +1,34 @@
+/*
+  
+  Solutions:
+    - Dynamic programming: O(n) time, O(n) space
+    - Dynamic programming + put dp array in nums: O(n) time, O(1) space
+  
+  Test cases:
+    - [1,2,3,1]: 4
+    - [2,7,9,3,1]: 12
+    - [5]: 5 (!!!)
+  
+  Testing grounds:
+    [2,7,9 ,3 ,1]
+    [2,7,11,11,12]
+         ^
+*/
+func rob(nums []int) int {
+  for i := 1; i < len(nums); i++ {
+    withRobbing := nums[i]
+    if i-2 >= 0 {
+      withRobbing += nums[i-2]
+    }
+    
+    withoutRobbing := nums[i-1]
+    
+    if withRobbing > withoutRobbing {
+      nums[i] = withRobbing
+    } else {
+      nums[i] = withoutRobbing
+    }
+  }
+  
+  return nums[len(nums)-1]
+}
