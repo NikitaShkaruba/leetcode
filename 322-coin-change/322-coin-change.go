11a2b8e4fd107e8f3543978ec74f93e2de168505
Nikita Shkaruba <sh.nickita@list.ru>
2022-03-15 03:22:59 +0300

    Time: 12 ms (82.40%), Space: 6.4 MB (93.27%) - LeetHub

---
+++ 322-coin-change/322-coin-change.go
@@ -0,0 +1,66 @@
+/*
+
+  Solutions:
+    1. dp: O(a*n) time, O(a) space, where n is the len of coins, and a is the amount
+    2. dp + only store max coin elements: O(a*n) time, O(max coin elements)
+    3. sort coins + dp + only store max coin elements: O(n*log(n) + a*n) time, O(n + max coin elements)
+  
+  Test cases:
+    - coins = [2, 5], amount = 3
+    - coins = [1,2,5], amount = 11
+    - coins = [2], amount = 3 (!!!)
+    - coins = [1], amount = 0 (!!!)
+  
+  Testing grounds:
+    [1,2,5], 11
+    
+     0,1,2,3,4,5,6,7,8,9,10,11
+    [0,1,1,2,2,1,2,2,3,3, 2, 3]
+                             ^
+                             
+     [1]
+     
+    [2, 5], 3
+    
+     0, 1, 2, 3
+    [0,-1, 1,-1]
+              ^
+                             ^
+*/
+func coinChange(coins []int, amount int) int {
+  dp := make([]int, 0, amount + 1)
+  for i := 0; i < amount + 1; i++ {
+    dp = append(dp, -1)
+  }
+  dp[0] = 0
+  
+  for i := 1; i <= amount; i++ {
+    for _, c := range coins {
+      diff := i - c
+      
+      if diff < 0 {
+        continue
+      }
+      
+      if dp[diff] == -1 {
+        continue 
+      }
+      
+      if dp[i] == -1 {
+        dp[i] = dp[diff] + 1
+      } else {
+        dp[i] = minInt(dp[i], dp[diff] + 1)
+      }
+    }
+  }
+  
+  return dp[len(dp)-1]
+}
+
+func minInt(a, b int) int {
+  if a < b {
+    return a
+  } else {
+    return b
+  }
+}
